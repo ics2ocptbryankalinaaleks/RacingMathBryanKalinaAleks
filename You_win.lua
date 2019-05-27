97f481e4759e5a2b977018d059a5cb1028bc5590
Aleksander Kucy <47784294+s429693@users.noreply.github.com>
2019-05-27 09:08:14 -0400

    added you lose & win

--- You_win.lua
+++ You_win.lua
@@ -1,23 +1,23 @@
 -----------------------------------------------------------------------------------------
--- level1_screen.lua
--- Created by: Aleks K.
--- Date: Month Day, Year
--- Description: This is the level 1 screen of the game.
+--
+-- SceneTemplate.lua
+-- Scene Template (Composer API)
+--
 -----------------------------------------------------------------------------------------
 
 -----------------------------------------------------------------------------------------
 -- INITIALIZATIONS
 -----------------------------------------------------------------------------------------
 
--- Use Composer Libraries
+-- Calling Composer Library
 local composer = require( "composer" )
-local widget = require( "widget" )
 
+local widget = require( "widget" )
 
 -----------------------------------------------------------------------------------------
 
 -- Naming Scene
-sceneName = "level1_screen"
+sceneName = "you_win"
 
 -----------------------------------------------------------------------------------------
 
@@ -25,112 +25,42 @@ sceneName = "level1_screen"
 local scene = composer.newScene( sceneName )
 
 -----------------------------------------------------------------------------------------
--- LOCAL VARIABLES
+-- FORWARD REFERENCES
 -----------------------------------------------------------------------------------------
 
--- The local variables for this scene
-local bkg_image
-local logocar = display.newImage("Images/CompanyLogo.png", 0, 0)
-local scrollSpeed = 1.4
-local scrollSpeed2 = -1.05
-local scrollSpeed3 = -1
-local scrollSpeed4 = -1.1
-local questionsAnswered = 0
+-- local variables for the scene
+local bkg
+-----------------------------------------------------------------
+--LOCAL SOUND
+-----------------------------------------------------------------
 
+----------------------------------------------------------------------------------------
 -----------------------------------------------------------------------------------------
--- LOCAL & GLOBAL SCENE FUNCTIONS
+-- LOCAL FUNCTIONS
 -----------------------------------------------------------------------------------------
 
-local function AskQuestion()
-    -- set all scroll speeds to 0 to stop the car
-    scrollSpeed = 0
-    scrollSpeed2 = 0
-    scrollSpeed3 = 0
-    scrollSpeed4 = 0
-    composer.showOverlay( "level1_question", { isModal = true, effect = "fade", time = 100})
-    questionsAnswered = questionsAnswered + 1
-end
-
-local function MovelogocarRight(event)
-    --print ("***MovelogocarRight: logocar.x = " .. logocar.x)
-    logocar.x = logocar.x + scrollSpeed
-end
-
-local function MovelogocarDown(event)
-    --print ("***MovelogocarDown: logocar.x = " .. logocar.x)
-    logocar.x = logocar.x - scrollSpeed3
-    logocar.y = logocar.y - scrollSpeed4
-
-
-    if (logocar.x >= 900) then
-        logocar:rotate(-45)
-        Runtime:removeEventListener("enterFrame", MovelogocarDown)
-        --Ask a question
-        AskQuestion()
-        Runtime:addEventListener("enterFrame", MovelogocarRight)
-    end
-end
-
-
-
-local function Movelogocar(event)
-    --print ("***Movelogocar: logocar.x = " .. logocar.x)
-    logocar.x = logocar.x + scrollSpeed
-    logocar.y = logocar.y + scrollSpeed2
-    
-
-    
-    if (logocar.x >= 657) then
-        logocar:rotate(90)
-        Runtime:removeEventListener("enterFrame", Movelogocar)
-        -- Ask another question
-        AskQuestion()
-        Runtime:addEventListener("enterFrame", MovelogocarDown)
-    end
-end
-
-
--------------------------------------------------------------
--- GLOBAL SCENE FUNCTIONS
--------------------------------------------------------------
-  
-function ResumeGame()
-    -- reset the speed
-    scrollSpeed = 1.4
-    scrollSpeed2 = -1.05
-    scrollSpeed3 = -1
-    scrollSpeed4 = -1.1
-    
-    if (questionsAnswered == 2) then
-        -- after getting 2 questions right, go to the you win screen
-        composer.gotoScene( "level2_screen" )
-    end
-end
-
------------------------------------------------------------------------------------------
--- GLOBAL SCENE FUNCTIONS
------------------------------------------------------------------------------------------
-
+--------------------------------------------------------------------------------------
 -- The function called when the screen doesn't exist
 function scene:create( event )
 
     -- Creating a group that associates objects with the scene
     local sceneGroup = self.view
 
-    -----------------------------------------------------------------------------------------
+    -- Display background
+    bkg = display.newImage("Images/YouWin.png")
+    bkg.x = display.contentCenterX
+    bkg.y = display.contentCenterY
+    bkg.width = display.contentWidth
+    bkg.height = display.contentHeight
+   
+    -- Associating display objects with this scene 
+    sceneGroup:insert( bkg )
+  
+end    
 
-    -- Insert the background image
-    bkg_image = display.newImageRect("Images/Level1Screen.png", display.contentWidth, display.contentHeight)
-    bkg_image.x = display.contentCenterX
-    bkg_image.y = display.contentCenterY
-    bkg_image.width = display.contentWidth
-    bkg_image.height = display.contentHeight
-
-    -- Insert background image into the scene group in order to ONLY be associated with this scene
-    sceneGroup:insert( bkg_image )    
-    sceneGroup:insert( logocar )
-
-end --function scene:create( event )
+-----------------------------------------------------------------------------------------
+-- GLOBAL SCENE FUNCTIONS
+-----------------------------------------------------------------------------------------
 
 -----------------------------------------------------------------------------------------
 
@@ -139,32 +69,29 @@ function scene:show( event )
 
     -- Creating a group that associates objects with the scene
     local sceneGroup = self.view
+
+    -----------------------------------------------------------------------------------------
+
     local phase = event.phase
 
     -----------------------------------------------------------------------------------------
 
     if ( phase == "will" ) then
-
         -- Called when the scene is still off screen (but is about to come on screen).
-        logocar.x = display.contentWidth*1/8
-        logocar.y = display.contentHeight*5.9/8
-        logocar:rotate(-45)
-        logocar:scale(0.1, 0.1)
+
     -----------------------------------------------------------------------------------------
 
     elseif ( phase == "did" ) then
 
+
+
         -- Called when the scene is now on screen.
         -- Insert code here to make the scene come alive.
         -- Example: start timers, begin animation, play audio, etc.
-
-
-        -- Ask a question
-
-        Runtime:addEventListener("enterFrame", Movelogocar)
-
     end
-end 
+
+end
+
 -----------------------------------------------------------------------------------------
 
 -- The function called when the scene is issued to leave the screen
@@ -172,7 +99,11 @@ function scene:hide( event )
 
     -- Creating a group that associates objects with the scene
     local sceneGroup = self.view
+
+    -----------------------------------------------------------------------------------------
+
     local phase = event.phase
+
     -----------------------------------------------------------------------------------------
 
     if ( phase == "will" ) then
@@ -184,11 +115,9 @@ function scene:hide( event )
 
     elseif ( phase == "did" ) then
         -- Called immediately after scene goes off screen.
-        Runtime:removeEventListener("enterFrame", Movelogocar)
-        Runtime:removeEventListener("enterFrame", MovelogocarDown)
     end
 
-end --function scene:hide( event )
+end
 
 -----------------------------------------------------------------------------------------
 
@@ -200,11 +129,11 @@ function scene:destroy( event )
 
     -----------------------------------------------------------------------------------------
 
+
     -- Called prior to the removal of scene's view ("sceneGroup").
     -- Insert code here to clean up the scene.
     -- Example: remove display objects, save state, etc.
-
-end -- function scene:destroy( event )
+end
 
 -----------------------------------------------------------------------------------------
 -- EVENT LISTENERS
@@ -215,6 +144,8 @@ scene:addEventListener( "create", scene )
 scene:addEventListener( "show", scene )
 scene:addEventListener( "hide", scene )
 scene:addEventListener( "destroy", scene )
+
 -----------------------------------------------------------------------------------------
 
 return scene
+
