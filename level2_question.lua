d02333b6e1e3a1c3a9e5ade95a577000e1a71035
Kalina Dunne Farrell <kalina.dunne-farrell@ocsbstudent.ca>
2019-05-30 15:32:12 -0400

    Update level2_question.lua

--- level2_question.lua
+++ level2_question.lua
@@ -906,18 +906,26 @@ local function AskQuestion9(sceneGroup)
     -- set the correct answer text, text color, and position (2nd)
     question9CorrectAnswer = display.newText("PURPLE", 0, 0, Arial, textSize)
     question9CorrectAnswer:setTextColor(127/255, 0, 1)
+    question9CorrectAnswer.x = 
+    question9CorrectAnswer.y = 
 
     -- set the first wrong answer text, text color, and position
     question9Answer1 = display.newText("BLUE", 0, 0, Arial, textSize)
     question9Answer1:setTextColor(0, 1, 0)
+    question9Answer1.x = 
+    question9Answer1.y = 
 
     -- set the second wrong answer text, text color, and position
     question9Answer2 = display.newText("YELLOW", 0, 0, Arial, textSize)
     question9Answer2:setTextColor(1, 1, 0)
+    question9Answer2.x = 
+    question9Answer2.y = 
 
     -- set the third wrong answer text, text color, and position
     question9Answer3 = display.newText("RED", 0, 0, Arial, textSize)
     question9Answer3:setTextColor(1, 0, 0)
+    question9Answer3.x = 
+    question9Answer3.y = 
 
     -- insert the text objects into this scene
     sceneGroup:insert(question9Text)
